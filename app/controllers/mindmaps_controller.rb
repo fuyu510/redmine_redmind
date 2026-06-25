2e14c76a089c6cd1d11a51d07580195fac511a70
ilkwon2 <ilkwon2@auto-it.co.kr>
2026-06-25 18:24:15 +0900

    Add save-back controller

---
+++ app/controllers/mindmaps_controller.rb
@@ -0,0 +1,47 @@
+require 'base64'
+
+class MindmapsController < ApplicationController
+  before_action :require_login
+
+  def update
+    object = RedmineRedmind::Registry.load_object(params[:object_type], params[:object_id])
+    return render_404 unless object
+    return render_403 unless RedmineRedmind::Registry.editable?(object, User.current)
+
+    field = RedmineRedmind::Registry.field_for(object)
+    source = object.send(field).to_s
+
+    begin
+      patched, found = RedmineRedmind::TextPatcher.replace_block(
+        source, decode_outline(params[:original]), params[:outline].to_s, params[:occurrence].to_i
+      )
+    rescue RedmineRedmind::TextPatcher::InvalidOutlineError
+      return render_invalid
+    end
+
+    return render_conflict unless found
+
+    if RedmineRedmind::Registry.persist(object, patched, User.current)
+      render json: { status: 'ok', outline: params[:outline].to_s }
+    else
+      render_invalid(object.errors.full_messages.join(', ').presence)
+    end
+  rescue ActiveRecord::StaleObjectError
+    render_conflict
+  end
+
+  private
+
+  def decode_outline(value)
+    Base64.decode64(value.to_s).force_encoding('UTF-8')
+  end
+
+  def render_conflict
+    render json: { status: 'conflict', message: l(:error_mindmap_conflict) }, status: :conflict
+  end
+
+  def render_invalid(message = nil)
+    render json: { status: 'error', message: message || l(:error_mindmap_invalid) },
+           status: :unprocessable_entity
+  end
+end
